421d539a3b875355d7da2e81cd6e6e4b38ee4f7b
Billy Price <williamp@microsoft.com>
2025-07-09 13:25:48 -0700

    show example usage with chrono types

--- examples/rt685s-evk/src/bin/rtc-time.rs
+++ examples/rt685s-evk/src/bin/rtc-time.rs
@@ -12,46 +12,60 @@ use {defmt_rtt as _, embassy_imxrt_examples as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+    const DEMO_DELAY_MS: u64 = 5000;
+
     let p = embassy_imxrt::init(Default::default());
-
     let mut r = Rtc::new(p.RTC);
-
     let (dt_clock, rtc_nvram) = r.split();
 
     // Datetime clock example
-    let datetime = Datetime::new(UncheckedDatetime {
-        year: 2024,
-        month: 10,
-        day: 4,
-        hour: 16,
-        ..Default::default()
-    })
-    .unwrap();
+    {
+        let datetime = Datetime::new(UncheckedDatetime {
+            year: 2024,
+            month: 10,
+            day: 4,
+            hour: 16,
+            ..Default::default()
+        })
+        .unwrap();
 
-    let ret = dt_clock.set_current_datetime(&datetime);
-    info!("RTC set time: {:?}", datetime);
-    // check if the set is valid
-    assert!(ret.is_ok());
+        let ret = dt_clock.set_current_datetime(&datetime);
+        info!("RTC set time: {:?}", datetime);
+        assert!(ret.is_ok());
 
-    info!("Wait for 5 seconds");
-    //wait for 5 seconds
-    Timer::after_millis(5000).await;
+        info!("Wait for 5 seconds");
+        Timer::after_millis(DEMO_DELAY_MS).await;
 
-    // get the datetime set and compare
-    let result = dt_clock.get_current_datetime();
-    assert!(result.is_ok());
-    info!("RTC get time: {:?}", result.unwrap());
+        let result = dt_clock.get_current_datetime();
+        assert!(result.is_ok());
+        info!("RTC get time: {:?}", result.unwrap());
+    }
 
     // NVRAM example
+    {
+        // Note that unused registers are handled with a '..' pattern, rather than explictly naming all registers.
+        // This approach is recommended, because it allows the code to remain compatible with optional HAL features that
+        // may consume some of these registers, so long as the total number of registers is not overcommitted.
+        // If the number of registers is overcommitted, the code will not compile, which will help catch the issue early.
+        let [gp0, gp1, ..] = rtc_nvram.storage();
+        info!("RTC NVRAM GP0: {:?}", gp0.read());
+        info!("RTC NVRAM GP1: {:?}", gp1.read());
 
-    // Note that unused registers are handled with a '..' pattern, rather than explictly naming all registers.
-    // This approach is recommended, because it allows the code to remain compatible with optional HAL features that
-    // may consume some of these registers, so long as the total number of registers is not overcommitted.
-    // If the number of registers is overcommitted, the code will not compile, which will help catch the issue early.
-    let [gp0, gp1, ..] = rtc_nvram.storage();
-    info!("RTC NVRAM GP0: {:?}", gp0.read());
-    info!("RTC NVRAM GP1: {:?}", gp1.read());
+        gp0.write(12345678);
+        info!("RTC NVRAM GP0: {:?}", gp0.read());
+    }
 
-    gp0.write(12345678);
-    info!("RTC NVRAM GP0: {:?}", gp0.read());
+    // Example of interop with the `chrono` library.  Requires the `chrono` feature to be enabled in the HAL and embedded-mcu crates.
+    {
+        let chrono_dt = chrono::NaiveDateTime::new(
+            chrono::NaiveDate::from_ymd_opt(2025, 6, 1).unwrap(),
+            chrono::NaiveTime::from_hms_opt(10, 30, 0).unwrap(),
+        );
+
+        let native_dt = Datetime::try_from(chrono_dt).expect(
+            "Conversion should always succeed because the date we provided above is within bounds (>= 1970-01-01).",
+        );
+
+        assert!(chrono::NaiveDateTime::from(native_dt) == chrono_dt);
+    }
 }
