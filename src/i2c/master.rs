a1a5ea2ba4bd8fe0d1246d7cdd28f1512fd87bed
Felipe Balbi <febalbi@microsoft.com>
2025-10-21 08:33:08 -0700

    i2c: master: filter to frequencies strictly less than bus speed
    Disallow configurations that would produce a nominal frequency equal
    to requested bus frequency. The reason for this is that all clocks
    used as I2C interface clock have some error margin in them and if they
    were to generate a frequency slightly higher than expected, we would
    end up with an out-of-spec frequency. To avoid that, generate a
    frequency slightly below target in hopes that errors will never cross
    that threshold.
    
    Signed-off-by: Felipe Balbi <febalbi@microsoft.com>

--- src/i2c/master.rs
+++ src/i2c/master.rs
@@ -126,7 +126,14 @@ impl SpeedRegisterSettings {
                 (hi_clocks, lo_clocks, clock_div_multiplier)
             })
             .filter(|(hi_clocks, lo_clocks, clock_div_multiplier)| {
-                get_freq_hz(*hi_clocks, *lo_clocks, *clock_div_multiplier, CLOCK_SPEED_HZ) <= target_freq_hz
+                // Require the resulting frequency to be strictly below `target_freq_hz`.
+                //
+                // This is necessary because hardware clock dividers and scalers can introduce
+                // rounding errors or imprecisions, causing the actual I2C clock to slightly exceed
+                // the calculated value. By enforcing a strict inequality, we ensure that the actual
+                // I2C clock frequency will not exceed the requested maximum, which is important for
+                // I2C compliance and to avoid violating timing requirements of connected devices.
+                get_freq_hz(*hi_clocks, *lo_clocks, *clock_div_multiplier, CLOCK_SPEED_HZ) < target_freq_hz
             })
             .min_by(|a, b| {
                 let (hi_a, lo_a, div_a) = a;
